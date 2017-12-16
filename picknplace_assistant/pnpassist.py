b2d7accc16ed37aa867b937a9a6ea7512da0bf0a
Dan Green <danngreen1@gmail.com>
2017-12-15 21:10:57 -0800

    Made text wrap, and added debug mode

--- picknplace_assistant/pnpassist.py
+++ picknplace_assistant/pnpassist.py
@@ -7,9 +7,11 @@ import pcbnew
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle, Circle, Ellipse, FancyBboxPatch
 
+import textwrap
 
 def create_board_figure(pcb, bom_row, layer=pcbnew.F_Cu):
     qty, value, footpr, highlight_refs = bom_row
+    # global ax
 
     plt.figure(figsize=(5.8, 8.2))
     ax = plt.subplot("111", aspect="equal")
@@ -39,12 +41,12 @@ def create_board_figure(pcb, bom_row, layer=pcbnew.F_Cu):
 
     # add title
     ax.text(board_xmin + .5 * (board_xmax - board_xmin), board_ymin - 0.5,
-            "%dx %s, %s" % (qty, value, footpr), #wrap=True,
-            horizontalalignment='center', verticalalignment='bottom')\
+            "%dx %s, %s" % (qty, value, footpr), 
+            horizontalalignment='center', verticalalignment='bottom')
 
     # add ref list
-    ax.text(board_xmin + .5 * (board_xmax - board_xmin), board_ymax + 0.5,
-            ", ".join(highlight_refs), #wrap=True,
+    ax.text((board_xmax + board_xmin)*0.5, board_ymax + 0.5,
+            textwrap.fill(", ".join(highlight_refs), 60), 
             horizontalalignment='center', verticalalignment='top')
 
     # draw parts
@@ -142,6 +144,9 @@ def generate_bom(pcb, filter_layer=None):
         refs.append(m.GetReference())
 
     # build bom table, sort refs
+    # bom_table is a list of lines
+    #    line is a tuple of (qty-int, value-string, fp-string, refs-[list])
+    #    refs is a list of references
     bom_table = []
     for (value, footpr), refs in part_groups.items():
         line = (len(refs), value, footpr, natural_sort(refs))
@@ -173,11 +178,23 @@ if __name__ == "__main__":
 
     # for each part group, print page to PDF
     #fname_out = os.path.splitext(args.file)[0] + "_picknplace.pdf"
-    fname_out = pcb.GetFileName() + "_picknplace.pdf"
-    with PdfPages(fname_out) as pdf:
-        for i, bom_row in enumerate(bom_table):
-            print("Plotting page (%d/%d)" % (i+1, len(bom_table)))
+    # if (int(sys.args[0])>0 && int(sys.args[0])<len(bom_table)):
+        # pagenum = int(sys.args[0]);
+    print_just_page_one = False
+    if print_just_page_one:
+        pagenum = 1
+        fname_out = pcb.GetFileName() + "_picknplace_"+str(pagenum)+".pdf"
+        with PdfPages(fname_out) as pdf:
+            bom_row = bom_table[pagenum-1]
+            print("Plotting page %d/%d" % (pagenum, len(bom_table)))
             create_board_figure(pcb, bom_row, layer=pcbnew.F_Cu)
             pdf.savefig()
-            #plt.close('') # This throws an error with wx when run interactively
+    else:
+        fname_out = pcb.GetFileName() + "_picknplace.pdf"
+        with PdfPages(fname_out) as pdf:
+            for i, bom_row in enumerate(bom_table):
+                print("Plotting page (%d/%d)" % (i+1, len(bom_table)))
+                create_board_figure(pcb, bom_row, layer=pcbnew.F_Cu)
+                pdf.savefig()
+                #plt.close('') # This throws an error with wx when run interactively
     print("Output written to %s" % fname_out)
