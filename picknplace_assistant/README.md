4a0e26bfd7f5b7908685bf792bf00a19bc21b6a9
Dan Green <danngreen1@gmail.com>
2019-06-06 09:20:55 -0700

    picknplace_assistant: added README, removed unused files, renamed kicad 6=>5.1

---
+++ picknplace_assistant/README.md
@@ -0,0 +1,7 @@
+Creates PDF files showing the placement of every component.
+
+Forked from https://gist.github.com/pwuertz/152f15b2dadca9a74039aeab944714af
+
+The pnpassist.py script can be run on the command line or in the PCB python terminal. The files in the plugin folders can be installed as plugins (see "How to install a plugin.md" file one directory up)
+
+__Todo: Fork this in github/gist instead of just copying and modifying__
--- picknplace_assistant/not working/additional_installs.sh
+++
@@ -1,7 +0,0 @@
-#Also need to do this, so it doesn't link against the System Python.
-# See https://stackoverflow.com/questions/15678153/homebrew-python-on-mac-os-x-10-8-fatal-python-error-pythreadstate-get-no-cu
-install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python /usr/local/Cellar/python/2.7.13/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib /Applications/Kicadnightly/kicad.app/Contents/Frameworks/python/site-packages/_pcbnew.so
-
-#install matplotlib
-#python2 -mpip install matplotlib
-
