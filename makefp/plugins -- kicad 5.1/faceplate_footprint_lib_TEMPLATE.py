b63e186c749bd414504ee7c861f76252070e9ffa
Dan Green <danngreen1@gmail.com>
2019-08-09 07:57:25 -0700

    makefp: Changed py file that returns location of footprints to be a template file (in order to hide local directories)

---
+++ makefp/plugins -- kicad 5.1/faceplate_footprint_lib_TEMPLATE.py
@@ -0,0 +1,8 @@
+
+def get_lib_location( ):
+
+	# - Rename this file faceplate_footprint_lib.py
+    # - Type the path to your 4ms_Faceplate.pretty footprint library (complete absolute path)
+
+	return "/Users/_your_username_here_/kicad/4ms-kicad-lib/lib-footprints/4ms_Faceplate.pretty"
+
