b5e2154a8771a856b3e13e3c93b98b9ba6287b2f
Dan Green <danngreen1@gmail.com>
2022-06-09 22:35:59 -0700

    resistor_gen: added E24 values to match Yageo's E96+E24 product lines

--- resistor_generator/resistor_gen.py
+++ resistor_generator/resistor_gen.py
@@ -1,40 +1,51 @@
 import sys
-import csv
 
 E96_base_values = [
-    1.00,	1.02,	1.05,
-    1.07,	1.10,	1.13,
-    1.15,	1.18,	1.21,
-    1.24,	1.27,	1.30,
-    1.33,	1.37,	1.40,
-    1.43,	1.47,	1.50,
-    1.54,	1.58,	1.62,
-    1.65,	1.69,	1.74,
-    1.78,	1.82,	1.87,
-    1.91,	1.96,	2.00,
-    2.05,	2.10,	2.16,
-    2.21,	2.26,	2.32,
-    2.37,	2.43,	2.49,
-    2.55,	2.61,	2.67,
-    2.74,	2.80,	2.87,
-    2.94,	3.01,	3.09,
-    3.16,	3.24,	3.32,
-    3.40,	3.48,	3.57,
-    3.65,	3.74,	3.83,
-    3.92,	4.02,	4.12,
-    4.22,	4.32,	4.42,
-    4.53,	4.64,	4.75,
-    4.87,	4.99,	5.11,
-    5.23,	5.36,	5.49,
-    5.62,	5.76,	5.90,
-    6.04,	6.19,	6.34,
-    6.49,	6.65,	6.81,
-    6.98,	7.15,	7.32,
-    7.50,	7.68,	7.87,
-    8.06,	8.25,	8.45,
-    8.66,	8.87,	9.09,
-    9.31,	9.53,	9.76,
+    1.00, 1.02, 1.05,
+    1.07, 1.10, 1.13,
+    1.15, 1.18, 1.21,
+    1.24, 1.27, 1.30,
+    1.33, 1.37, 1.40,
+    1.43, 1.47, 1.50,
+    1.54, 1.58, 1.62,
+    1.65, 1.69, 1.74,
+    1.78, 1.82, 1.87,
+    1.91, 1.96, 2.00,
+    2.05, 2.10, 2.16,
+    2.21, 2.26, 2.32,
+    2.37, 2.43, 2.49,
+    2.55, 2.61, 2.67,
+    2.74, 2.80, 2.87,
+    2.94, 3.01, 3.09,
+    3.16, 3.24, 3.32,
+    3.40, 3.48, 3.57,
+    3.65, 3.74, 3.83,
+    3.92, 4.02, 4.12,
+    4.22, 4.32, 4.42,
+    4.53, 4.64, 4.75,
+    4.87, 4.99, 5.11,
+    5.23, 5.36, 5.49,
+    5.62, 5.76, 5.90,
+    6.04, 6.19, 6.34,
+    6.49, 6.65, 6.81,
+    6.98, 7.15, 7.32,
+    7.50, 7.68, 7.87,
+    8.06, 8.25, 8.45,
+    8.66, 8.87, 9.09,
+    9.31, 9.53, 9.76,
 ]
+
+#Values in E24 that aren't also in E96
+extra_E24_base_values = [
+    1.20, 1.60, 1.80, 
+    2.20, 2.40, 2.70, 
+    3.00, 3.30, 3.60, 
+    3.90, 4.30, 4.70, 
+    5.10, 5.60, 6.20, 
+    6.80, 8.20, 9.10
+]
+
+E96_plus_E24_values = E96_base_values + extra_E24_base_values
 
 multiplier_list = [
     1,
@@ -186,7 +197,7 @@ if __name__ == "__main__":
         print("""
     Usage: python3 resistor_gen.py libfilename {package} {tolerance} {min_mult} {max_mult} 
 
-    Generates a Kicad 6 symbol library or E96 resistors for a given
+    Generates a Kicad 6 symbol library or E96+E24 resistors for a given
     package size. Yageo RC-series resistor part numbers will be added
     to each symbol's Part Number field.
 
@@ -206,7 +217,7 @@ if __name__ == "__main__":
     elif outfile=="print-partnums":
         tpl = "%PARTNUM%"
         for m in multiplier_list[multiplier_list.index(minmult):multiplier_list.index(maxmult)+1]:
-            for v in E96_base_values:
+            for v in E96_plus_E24_values:
                 val = m * v
                 print(gen_res(jlc, val, package, tolerance, tpl))
 
@@ -214,7 +225,7 @@ if __name__ == "__main__":
         print('"Comment", "Designator", "Footprint", "JLCPCB Part #"')
         i = 0
         for m in multiplier_list[multiplier_list.index(minmult):multiplier_list.index(maxmult)+1]:
-            for v in E96_base_values:
+            for v in E96_plus_E24_values:
                 val = m * v
                 tpl = f"\"%VAL% %PKG% %TOL%\", \"R{i}\", \"R%PKG%\",  \"%JLCPCBID%\""
                 i = i + 1
@@ -233,11 +244,9 @@ if __name__ == "__main__":
         with open(template_file) as tpl:
             tpl_data = tpl.read()
             for m in multiplier_list:
-                if m < minmult:
+                if m < minmult or m > maxmult:
                     continue
-                if m > maxmult:
-                    continue
-                for v in E96_base_values:
+                for v in E96_plus_E24_values:
                     val = m * v
                     libdata += gen_res(jlc, val, package, tolerance, tpl_data)
             libdata += footer
